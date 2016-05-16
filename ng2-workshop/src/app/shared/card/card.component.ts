d3ad872f623c414585333883ae4356528573caf9
Matt Robbins <matthew.r.robbins@gmail.com>
2016-05-16 17:12:57 -0500

    proved edit is not special keyword

--- ng2-workshop/src/app/shared/card/card.component.ts
+++ ng2-workshop/src/app/shared/card/card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() person;
-  @Output() edit = new EventEmitter();
+  @Output() editBtn = new EventEmitter();
   constructor() {}
 
   ngOnInit() {
