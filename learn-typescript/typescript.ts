189d696ab42560f749409d2955ec37962da1a000
Matt Robbins <matthew.r.robbins@gmail.com>
2016-05-16 15:45:04 -0500

    More typescript

--- learn-typescript/typescript.ts
+++ learn-typescript/typescript.ts
@@ -1,7 +1,25 @@
-let arry:number[] = [1, 2];
+var arry = [1, 2];
 
-function add(first: number, second: number) {
+function add(first, second) {
     return first + second;
 }
 
-console.log(add(...arry));
+console.log(add(1,2));
+/*console.log(add(...arry));*/
+
+interface Vechicle {
+    wheels:number;
+    towing:boolean;
+}
+
+class Truck implements Vechicle {
+    length:number;
+
+    constructor(public wheels:number, public towing:boolean) {}
+
+}
+
+console.log(
+`Truck has below number of wheels:
+${new Truck(18, false).wheels}`
+);
